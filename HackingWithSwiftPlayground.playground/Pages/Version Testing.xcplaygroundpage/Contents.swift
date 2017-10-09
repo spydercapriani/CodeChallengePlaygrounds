3edd1916c63a1304e59ae358ff81a5e5ecbb0972
Danny Gilbert <gilbert.daniel@heb.com>
2017-10-09 10:49:49 -0500

    Separated POP Example into own playground page

--- HackingWithSwiftPlayground.playground/Pages/Version Testing.xcplaygroundpage/Contents.swift
+++ HackingWithSwiftPlayground.playground/Pages/Version Testing.xcplaygroundpage/Contents.swift
@@ -1,8 +1,6 @@
 //: [Previous](@previous)
 
 import Foundation
-import UIKit
-import PlaygroundSupport
 
 func getLatestVersion(version1: String, version2: String) -> String {
     let version1details = version1.components(separatedBy: ".")
@@ -24,114 +22,6 @@ assert(getLatestVersion(version1: "12.14.10", version2: "12.10.12") == "12.14.10
 assert(getLatestVersion(version1: "12.14.10a", version2: "12.14.10c") == "12.14.10c", "Version Challenge failed!")
 assert(getLatestVersion(version1: "12.14.10", version2: "12.14.10") == "same versions", "Version Challenge failed!")
 print("Tests completed successfully!")
-
-//: Abstract Classes vs Interfaces
-
-/*:
- # Interfaces
- * Only events, delegates, properties, and methods can exist
- * Classes can implement multiple interfaces
- * Interfaces would be how two objects talk with one another in object-oriented programming
- 
- # Abstract Classes
- * Ability to specify default implementations of methods
- * Have slightly more control in how 'blueprint' methods are called
- * Able to provide behaviors related/unrelated to the interface
- * Can only subclass one abstract class
- 
- ****  VS  ****
- 
- # Protocol Oriented Programming (POP)
- * Traits - A distinguishing quality or characteristic
- * Abilities - The concept of extending protocols to give them default implementations
- 
- Traits can and should have abilities. Protocols can act as a traits or an interface.
- We want to create protocols that represent traits our objects can have. We then want to
- give our traits abilities through the use of protocol extensions and constraints.
- In Swift, protocols tend to lend themselves more to the idea of a trait (a characteristic
- or quality of an object) rather than just to an interface (a contract for common communication
- for an object).
- 
- */
-
-//: Example of taking a POP approach
-public protocol canBlockView {
-    var view: UIView! { get set }
-    func blockUI()
-    func unblockUI()
-}
-
-public extension canBlockView {
-    func blockUI() {
-        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
-        let blockingView = UIView(frame: .zero)
-        
-        blockingView.tag = 100
-        activityIndicator.tag = 101
-        
-        blockingView.backgroundColor = .black
-        blockingView.alpha = CGFloat(0.85)
-        
-        activityIndicator.color = .white
-        
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        blockingView.translatesAutoresizingMaskIntoConstraints = false
-        
-        blockingView.addSubview(activityIndicator)
-        view.addSubview(blockingView)
-        
-        NSLayoutConstraint.activate(
-            constraints(for: activityIndicator, on: blockingView) +
-                constraints(for: blockingView)
-        )
-        
-        activityIndicator.startAnimating()
-    }
-    
-    func unblockUI() {
-        if let blockingView = self.view.viewWithTag(100), let activityIndicator = self.view.viewWithTag(101){
-            blockingView.removeFromSuperview()
-            activityIndicator.removeFromSuperview()
-        }
-    }
-    
-    private func constraints(for indicator: UIActivityIndicatorView, on blockingView: UIView) -> [NSLayoutConstraint] {
-        return [
-            indicator.centerXAnchor.constraint(equalTo: blockingView.centerXAnchor),
-            indicator.centerYAnchor.constraint(equalTo: blockingView.centerYAnchor)
-        ]
-    }
-    
-    private func constraints(for blockingView: UIView) -> [NSLayoutConstraint] {
-        return [
-            blockingView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-            blockingView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            blockingView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-            blockingView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
-        ]
-    }
-}
-
-class ViewContainer: canBlockView {
-    var view: UIView!
-    
-    init(view: UIView) {
-        self.view = view
-    }
-}
-
-let view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 640))
-view.backgroundColor = .white
-PlaygroundPage.current.liveView = view
-
-let container = ViewContainer(view: view)
-
-container.blockUI()
-let exampleNetworkCallTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false, block: { _ in
-    container.unblockUI()
-})
-
-
 
 
 //: [Next](@next)
