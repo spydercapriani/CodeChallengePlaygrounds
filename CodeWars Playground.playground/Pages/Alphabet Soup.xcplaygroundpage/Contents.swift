024ae4d03643c42464d08abe20ff9cc1f4945d2f
Danny Gilbert <danny_gilbert@icloud.com>
2017-11-20 16:06:25 -0600

    added tests to alphabet soup

---
+++ CodeWars Playground.playground/Pages/Alphabet Soup.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func AlphabetSoup(_ str: String) -> String {
+    return String(str.sorted())
+}
+
+AlphabetSoup("coderbyte") == "bcdeeorty"
+AlphabetSoup("hooplah") == "ahhloop"
+AlphabetSoup("love") == "elov"
+AlphabetSoup("yoop") == "oopy"
+AlphabetSoup("comma") == "acmmo"
+//: [Next](@next)
