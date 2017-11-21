3e46d06e5180573e44e686a5204b01573bc501d3
Danny Gilbert <danny_gilbert@icloud.com>
2017-11-21 13:41:00 -0600

    Challenge 17 Random Number Generator

--- HackingWithSwiftPlayground.playground/Pages/Number Challenges.xcplaygroundpage/Contents.swift
+++ HackingWithSwiftPlayground.playground/Pages/Number Challenges.xcplaygroundpage/Contents.swift
@@ -26,4 +26,20 @@ assert(fizzTest[5] == "Buzz", "Challenge 16 Failed!")
 assert(fizzTest[15] == "FizzBuzz", "Challenge 16 Failed!")
 print("Challenge 16 completed!")
 
+/*: # Challenge 17: Generate a Random Number in a range
+ Write a function that accepts positive minimum and maximum integers,
+ and returns a random number between those two bounds, inclusive.
+ */
+func randomNumberGenerator(min: Int, max: Int) -> Int {
+    return min + Int(arc4random_uniform(UInt32(max - min)))
+}
+
+randomNumberGenerator(min: 1, max: 5)
+
+assert((1...5).contains(randomNumberGenerator(min: 1, max: 5)), "Challenge 17 Failed!")
+assert((8...10).contains(randomNumberGenerator(min: 8, max: 10)), "Challenge 17 Failed!")
+assert(randomNumberGenerator(min: 12, max: 12) == 12, "Challenge 17 Failed!")
+assert(randomNumberGenerator(min: 12, max: 18) != 7, "Challenge 17 Failed!")
+print("Challenge 17 completed!")
+
 //: [Next](@next)
